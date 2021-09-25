d647ecf2e00c76214c8ec4272e34d6a5793b69e1
sarthu858 <sarthu858@gmail.com>
2021-09-25 21:56:05 +0530

    Week 6 Added

--- week2/3ques.cpp
+++ week2/3ques.cpp
@@ -8,34 +8,56 @@ const int N =  1e6 + 5;
 
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        vector<int> hsh(N,0);
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        rep(i, 0, n) {
-            cin >> a[i];
-            hsh[a[i]] ++;
-        }
-        int k;
-        cin >> k;
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
+    //     vector<int> hsh(N,0);
+    //     int n;
+    //     cin >> n;
+    //     vector<int> a(n);
+    //     rep(i, 0, n) {
+    //         cin >> a[i];
+    //         hsh[a[i]] ++;
+    //     }
+    //     int k;
+    //     cin >> k;
 
-        int cnt = 0;
+    //     int cnt = 0;
 
-        rep(i,0,n) {
-            hsh[a[i]]--;
-            if(hsh[(a[i] + k)]) {
-                cnt++;
-            }
-            if(k) hsh[a[i]]++;
-        }
+    //     rep(i,0,n) {
+    //         hsh[a[i]]--;
+    //         if(hsh[(a[i] + k)]) {
+    //             cnt++;
+    //         }
+    //         if(k) hsh[a[i]]++;
+    //     }
 
-        cout << cnt << endl;
+    //     cout << cnt << endl;
         
 
+    // }
+   	string s = "";
+    stack<string> st; st.push(s);
+    int q; cin >> q;
+    while(q--) {
+        int n; cin >> n;
+        if(n == 1) {
+            string w; cin >> w;
+            s += w;
+            st.push(s);
+        }else if(n == 2) {
+            int k; cin >> k;
+            s = s.substr(0, s.size() - k);
+            st.push(s);
+        }else if(n == 3) {
+            int k; cin >> k;
+            cout << s[k - 1] << endl;
+        }else {
+            s = st.top();
+            st.pop();
+        }
+        cout << s << endl;
     }
     return 0;
 }
